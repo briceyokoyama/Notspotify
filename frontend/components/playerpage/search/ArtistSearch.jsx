8eeed5a762231832673c5d465ecee001278218a6
briceyokoyama <briceyokoyama@gmail.com>
2019-06-18 11:30:30 -0700

    Stlye artist search results

--- frontend/components/playerpage/search/ArtistSearch.jsx
+++ frontend/components/playerpage/search/ArtistSearch.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ArtistIndexItem from '../display/indexes/artists/ArtistIndexItem';
 
 export default (props) => (
-  <div className='aritst-index'>
+  <div className='artist-index'>
     <ul>
       {props.artists.map(artist => <ArtistIndexItem key={artist.id} artist={artist} />)}
     </ul>
