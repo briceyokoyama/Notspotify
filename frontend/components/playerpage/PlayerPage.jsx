2c9d29de46a90add267f926101637af78d5e9dd2
briceyokoyama <briceyokoyama@gmail.com>
2019-06-18 23:04:43 -0700

    resolved merge conflicts with add_search

--- frontend/components/playerpage/PlayerPage.jsx
+++ frontend/components/playerpage/PlayerPage.jsx
@@ -4,6 +4,7 @@ import PlayerNavBarContainer from './navbar/PlayerNavBarContainer';
 import PlayBarContainer from './playbar/PlayBarContainer';
 import PlaylistShowContainer from './display/shows/playlist/PlaylistShowContainer';
 import PlayerDisplay from './display/PlayerDisplay';
+import SearchContainer from './search/SearchContainer';
 
 const PlayerPage = () => {
   return (
@@ -11,11 +12,8 @@ const PlayerPage = () => {
       <PlayerNavBarContainer />
       <PlayBarContainer />
       <Switch>
-<<<<<<< HEAD
         <Route path='/search/results/:searchTerm' component={SearchContainer} />
         <Route path='/search' component={SearchContainer} />
-=======
->>>>>>> master
         <Route path='playlists/:playlistId' component={PlaylistShowContainer} />
         <Route path='/' component={PlayerDisplay} />
       </Switch>
--- frontend/components/playerpage/display/indexes/playlists/PlaylistIndexContainer.js
+++ frontend/components/playerpage/display/indexes/playlists/PlaylistIndexContainer.js
@@ -15,13 +15,8 @@ const playlistSelector = (playlists, scenario, user_id, playlistFollowers) => {
 
 const mstp = ({entities: {playlists, playlistFollowers}, session: {id}}, ownProps) => ({
   currentUserId: id,
-<<<<<<< HEAD
   playlists: playlistSelector(playlists, ownProps.match.params.main || 'library', id, playlistFollowers),
   // scenario: (ownProps.adding === true) ? 'library' : ownProps.match.params.main
-=======
-  playlists: playlistSelector(playlists, ownProps.match.params.main, id, playlistFollowers),
-  scenario: ownProps.match.params.main
->>>>>>> master
 })
 
 const mdtp = dispatch => ({
@@ -30,4 +25,4 @@ const mdtp = dispatch => ({
   fetchPlaylistFollowers: () => dispatch(fetchPlaylistFollowers())
 })
 
-export default withRouter(connect(mstp, mdtp)(PlaylistIndex));
+export default connect(mstp, mdtp)(PlaylistIndex);
