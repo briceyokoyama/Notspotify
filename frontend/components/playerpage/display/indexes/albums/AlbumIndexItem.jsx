eedeaa130adcfde3d48a65ccdbaa266ea841fc39
briceyokoyama <briceyokoyama@gmail.com>
2019-06-19 21:10:01 -0700

    Add correct links from index pages

--- frontend/components/playerpage/display/indexes/albums/AlbumIndexItem.jsx
+++ frontend/components/playerpage/display/indexes/albums/AlbumIndexItem.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 export default (props) => (
   <li>
     <div className='album-index-item'>
-      <Link to={`/home/albums/${props.album.id}`}>
+      <Link to={`/albums/${props.album.id}`}>
         <img className='album-image' src={props.album.coverUrl} alt="album"/>
       </Link>
-      <Link to={`/home/albums/${props.album.id}`}>
+      <Link to={`/albums/${props.album.id}`}>
         <span className='album-name'>{props.album.title}</span>
       </Link>
     </div>
--- frontend/components/playerpage/display/indexes/artists/ArtistIndexItem.jsx
+++ frontend/components/playerpage/display/indexes/artists/ArtistIndexItem.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 export default (props) => (
   <li>
     <div className='artist-index-item'>
-      <Link to={`/home/artists/${props.artist.id}`}>
+      <Link to={`/artists/${props.artist.id}`}>
         <img className='artist-image' src={props.artist.avatarUrl} alt="playlist"/>
       </Link>
-      <Link to={`/home/artists/${props.artist.id}`}>
+      <Link to={`/artists/${props.artist.id}`}>
         <span className='artist-name'>{props.artist.name}</span>
       </Link>
     </div>
